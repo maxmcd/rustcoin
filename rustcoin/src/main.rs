cdc9c287139ce4c6803fd67f293582c8b7afae91
maxmcd <max.t.mcdonnell@gmail.com>
2018-01-30 18:20:01 -0500

    Completed address and key generation

--- rustcoin/src/main.rs
+++ rustcoin/src/main.rs
@@ -2,11 +2,18 @@ extern crate crypto;
 extern crate rand;
 extern crate secp256k1;
 extern crate time;
+extern crate core;
+extern crate rust_base58;
+
 
 use rand::OsRng;
+use rand::Rng;
 use std::time::{SystemTime, UNIX_EPOCH};
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use crypto::ripemd160::Ripemd160;
+use core::ops::Index;
+use rust_base58::{ToBase58};
 
 fn main() {
     let index = 0;
@@ -17,11 +24,47 @@ fn main() {
     let hash = calculate_hash(index, &prev_hash, &data, timestamp, nonce);
     println!("{}", hash);
     mine(&hash, index);
+    generate_new_address_and_keys()
+}
+
+fn generate_new_address_and_keys() {
+    // https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
     let secp = secp256k1::Secp256k1::new();
     let mut rng = OsRng::new().expect("Failed to create new rng");
     let (secret_key, public_key) = secp.generate_keypair(&mut rng).unwrap();
     println!("{:?}", secret_key);
     println!("{:?}", public_key);
+
+    let mut result_bytes = sha_256_bytes(secret_key.index(0..32));
+    println!("{:?}", "sha of public key");
+    println!("{:?}", result_bytes);
+
+    let mut rmd160 = Ripemd160::new();
+    rmd160.input(&mut result_bytes);
+    let mut rmd160_result = [0; 20];
+    rmd160.result(&mut rmd160_result);
+    println!("{:?}", "rmd160 of sha of public key");
+    println!("{:?}", rmd160_result);
+
+    let version = [0];
+    let with_version = [&version[0..1], &rmd160_result[0..20]].concat();
+    println!("{:?}", with_version);
+
+    let sha_twice = sha_256_bytes(&sha_256_bytes(&with_version));
+    println!("{:?}", sha_twice);
+
+    let address = [&with_version[0..21], &sha_twice[0..4]].concat();
+    println!("{:?}", address);
+
+    println!("{:?}", &address.to_base58());
+}
+
+fn sha_256_bytes(bytes: &[u8]) -> [u8; 32] {
+    let mut sha = Sha256::new();
+    sha.input(bytes);
+    let mut out = [0; 32];
+    sha.result(&mut out);
+    out
 }
 
 fn calculate_hash(
